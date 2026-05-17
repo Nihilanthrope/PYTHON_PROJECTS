ab2ed67f7d69fffb00d1dec74da3b934f40ea5d7
JUNIOR CAN'T CODE <rebux690@gmail.com>
2026-05-17 18:42:33 +0530

    Add files via upload

---
+++ Bhangbhosda1.py
@@ -0,0 +1,19 @@
+a = int(input("Entery Number 1: "))
+
+b= int(input("Enter Number 2 : "))
+
+
+
+print ("Sum in + ", a+b)
+
+print ("Sum in - ", a-b)
+
+print ("Sum in × ", a*b)
+
+print ("Sum in % ", a%b)
+
+
+"""KHUD KA BHANG BHODSA DIMAG USE KRTA HUA ME 🥹🤙
+ haa bhai pagal ho gaya hu me😭💔 
+ kch nhi krleta hu!!"""
+ 
