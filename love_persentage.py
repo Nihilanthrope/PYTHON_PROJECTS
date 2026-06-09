6ed05b902447ac06bbe57ce395e174fe5e20bab5
Nihilanthrope <rebux690@gmail.com>
2026-06-09 23:04:02 +0530

    Update love_persentage.py

---
+++ love_persentage.py
@@ -0,0 +1,35 @@
+name1 = input("Write Your Name :- ").lower()
+name2 = input ("Write Your Crush Name :- ").lower()
+name = name1+name2
+ankita=name
+
+
+love_count = (
+
+    name.count("l") +
+    name.count("o") +
+    name.count("v") +
+    name.count("e")
+    
+)
+
+A = love_count * 10 
+
+if (name1 == "saori" and name2 == "honey") or (name1 == "honey" and name2 == "saori"):
+	print ("Your Made For Each Other Best Couples For sure I'm rooting For You guys🥹 I hope you guys get married AsAp")
+
+
+else:
+	print(f"Your Love % is {A}")
+
+#for Her in word:
+   #print (Her, "=", word.count(Her))
+    
+    
+print("<<<=============================================================================================>>>")
+print("|||	           I LOVE ANKITA YALLL  			                	                            |||")
+print("|||	 BROO SHESS SOO CUTEEWW🥹	                                                                |||")
+print("|||	IMAA MARRY HER YALLL🥹 SHES DAAA BESESTTTT				    	                            |||")
+print("|||			😭THIS IS MY FAVVV PROJECT FOR SUREEE				                                |||")
+print("|||	WHENEVER I THINK ABT HER I GET GULULULULU OR BAWHAWWNAWOS 😭😭😭 JUST UNDERSTAND NAAAAA u   |||")         								
+print("<<<=============================================================================================>>>")
