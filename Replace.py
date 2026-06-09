3058acd50aaa881ba6cb9edf66901a225884ae6e
Nihilanthrope <rebux690@gmail.com>
2026-06-09 16:40:38 +0530

    Add files via upload

---
+++ Replace.py
@@ -0,0 +1,6 @@
+snt = (input("Enter Your Sentence:- "))
+print (f"Your Sentance Is {snt}")
+
+word1= (input("Enter Your Word You Want To replace:- "))
+word2 = (input("Enter The Word You Want To Replace With :- "))
+print (snt.replace(word1,word2))
